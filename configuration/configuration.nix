9dec162001b39f5471eba3b31474045503629886
jigsaw <phattarawat12cg@gmail.com>
2025-10-23 12:57:28 +0700

    divied into modules

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -9,6 +9,9 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.home-manager
+      ./modules/desktop.nix
+      ./modules/packages.nix
+      ./modules/system.nix
     ];
 
   # Bootloader.
@@ -24,103 +27,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "Asia/Bangkok";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "th_TH.UTF-8";
-    LC_IDENTIFICATION = "th_TH.UTF-8";
-    LC_MEASUREMENT = "th_TH.UTF-8";
-    LC_MONETARY = "th_TH.UTF-8";
-    LC_NAME = "th_TH.UTF-8";
-    LC_NUMERIC = "th_TH.UTF-8";
-    LC_PAPER = "th_TH.UTF-8";
-    LC_TELEPHONE = "th_TH.UTF-8";
-    LC_TIME = "th_TH.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.jigsaw = {
-    isNormalUser = true;
-    description = "jigsaw";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    ];
-   };
-
-  #Auto garbage collection
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 14d";
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    zed-editor
-    brave
-    btop
-    ghostty
-    git
-    bat
-    tealdeer
-    fastfetch
-    discord
-    home-manager
-  ];
-
-  #experimental-feature
-  nix.settings.experimental-features = ["nix-command" "flakes"];
 
   #home-manager
   home-manager = {
